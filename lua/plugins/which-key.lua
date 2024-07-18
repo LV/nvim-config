db7ce6660212d2d9bba96766c039a9cd7da2f763
Luis Victoria <v@lambda.lv>
2024-07-18 02:40:16 -0400

    Reformat `which-key` to prepend spaces

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -16,13 +16,13 @@ return {
       -- LSP
       { "<leader>c", group = "code" },
         -- clangd
-      {"<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", desc = "Code action" },
-      {"<leader>cc", "<cmd>lua vim.lsp.buf.hover()<CR>", desc = "Show code context" },
-      {"<leader>cd", "<cmd>lua vim.lsp.buf.definition()<CR>", desc = "Go to definition" },
-      {"<leader>cD", "<cmd>lua vim.lsp.buf.declaration()<CR>", desc = "Go to declaration" },
-      {"<leader>ci", "<cmd>lua vim.lsp.buf.implementation()<CR>", desc = "Go to implementation" },
-      {"<leader>cr", "<cmd>lua vim.lsp.buf.references()<CR>", desc = "Find references" },
-      {"<leader>cR", "<cmd>lua vim.lsp.buf.rename()<CR>", desc = "Rename symbol" },
+      { "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", desc = "Code action" },
+      { "<leader>cc", "<cmd>lua vim.lsp.buf.hover()<CR>", desc = "Show code context" },
+      { "<leader>cd", "<cmd>lua vim.lsp.buf.definition()<CR>", desc = "Go to definition" },
+      { "<leader>cD", "<cmd>lua vim.lsp.buf.declaration()<CR>", desc = "Go to declaration" },
+      { "<leader>ci", "<cmd>lua vim.lsp.buf.implementation()<CR>", desc = "Go to implementation" },
+      { "<leader>cr", "<cmd>lua vim.lsp.buf.references()<CR>", desc = "Find references" },
+      { "<leader>cR", "<cmd>lua vim.lsp.buf.rename()<CR>", desc = "Rename symbol" },
 
       { "<leader>f", group = "file" },
       { "<leader>g", group = "git" },
@@ -30,8 +30,8 @@ return {
       -- Template
       { "<leader>t", group = "template" },
       { "<leader>tc", group = "cpp" },
-      {"<leader>tch", "<cmd>Template cpp/header<CR>", desc = "Load C++ header template" },
-      {"<leader>tcs", "<cmd>Template cpp/source()<CR>", desc = "Load C++ source template" },
+      { "<leader>tch", "<cmd>Template cpp/header<CR>", desc = "Load C++ header template" },
+      { "<leader>tcs", "<cmd>Template cpp/source()<CR>", desc = "Load C++ source template" },
     },
   },
 }
