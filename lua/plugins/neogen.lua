d64bd839c6f5bb69abb956a1776f8933462c4889
Luis Victoria <v@lambda.lv>
2024-07-27 01:00:51 -0400

    Add `neogen` and `nvim-treesitter`

---
+++ lua/plugins/neogen.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "danymat/neogen",
+    config = function()
+      require("neogen").setup({
+        enabled = true,
+        languages = {
+          cpp = {
+            template = {
+              annotation_convention = "doxygen"
+            },
+          },
+        },
+      })
+      local opts = { noremap = true, silent = true }
+      vim.api.nvim_set_keymap("n", "<Leader>dd", ":Neogen<CR>", opts)
+      vim.api.nvim_set_keymap("n", "<Leader>df", ":Neogen func<CR>", opts)
+      vim.api.nvim_set_keymap("n", "<Leader>dc", ":Neogen class<CR>", opts)
+      vim.api.nvim_set_keymap("n", "<Leader>dt", ":Neogen type<CR>", opts)
+      vim.api.nvim_set_keymap("n", "<Leader>dl", ":Neogen file<CR>", opts)
+    end,
+    requires = "nvim-treesitter/nvim-treesitter",
+    ft = { "cpp" }, -- Load only for C++ files
+  }
+}
+
---
+++ lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,45 @@
+local config = function()
+  require("nvim-treesitter.configs").setup({
+    build = ":TSUpdate",
+    indent = {
+      enable = true,
+    },
+    autotag = {
+      enable = true,
+    },
+    event = {
+      "BufReadPre",
+      "BufNewFile",
+    },
+    ensure_installed = {
+      "cpp",
+      "dockerfile",
+      "gitignore",
+      "lua",
+      "markdown",
+      "python",
+      "toml",
+      "yaml",
+    },
+    auto_install = true,
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = true,
+    },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<C-s>",
+        node_incremental = "<C-s>",
+        scope_incremental = false,
+        node_decremental = "<BS>",
+      },
+    },
+  })
+end
+
+return {
+  "nvim-treesitter/nvim-treesitter",
+  lazy = false,
+  config = config,
+}
