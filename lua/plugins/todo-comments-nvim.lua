727c37758d72c7d98980ac560cbd2e0680b6fc0e
Luis Victoria <v@lambda.lv>
2024-08-20 23:27:08 -0400

    Add todo-comments search to Telescope

--- lua/plugins/todo-comments-nvim.lua
+++ lua/plugins/todo-comments-nvim.lua
@@ -1,7 +1,7 @@
 return {
   "folke/todo-comments.nvim",
   event = { "BufReadPost", "BufNewFile" },
-  dependencies = { "nvim-lua/plenary.nvim" },
+  dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
   opts = {
     -- your configuration comes here
     -- or leave it empty to use the default settings
