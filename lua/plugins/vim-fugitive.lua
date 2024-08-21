5fde0da33e1a73f2e18ed1803db76b5bdf61287d
Luis Victoria <v@lambda.lv>
2024-08-20 22:20:50 -0400

    Add `vim-fugitive`

---
+++ lua/plugins/vim-fugitive.lua
@@ -0,0 +1,10 @@
+return {
+  "tpope/vim-fugitive",
+  lazy = true,
+  cmd = { "G", "Git" },
+  keys = {
+    { "<leader>gb", "<cmd>Git blame<CR>", desc = "Git Blame" },
+    { "<leader>gd", "<cmd>Git diff<CR>", desc = "Git Diff" },
+    -- More commands here: https://github.com/tpope/vim-fugitive
+  },
+}
