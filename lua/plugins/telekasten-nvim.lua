9e3db0ae8fa95e8d98199a14f4b7dbc3ee277f67
Luis Victoria <v@lambda.lv>
2024-09-12 15:25:12 -0400

    Fix missing comma bug

--- lua/plugins/telekasten-nvim.lua
+++ lua/plugins/telekasten-nvim.lua
@@ -10,7 +10,7 @@ return {
   ft = "markdown",
   dependencies = { "nvim-telescope/telescope.nvim" },
   keys = {
-    { "<leader>znn", "<cmd>Telekasten new_note<CR>", desc = "New Note" }
+    { "<leader>znn", "<cmd>Telekasten new_note<CR>", desc = "New Note" },
     { "<leader>zp", "<cmd>Telekasten panel<CR>", desc = "Panel" },
   },
   config = config,
