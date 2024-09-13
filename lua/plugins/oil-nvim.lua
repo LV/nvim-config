df5ce6c047cde7645ab913c66c3667c3065db795
Luis Victoria <v@lambda.lv>
2024-09-12 23:14:57 -0400

    Replace `telescope-file-browser` with `oil.nvim`

---
+++ lua/plugins/oil-nvim.lua
@@ -0,0 +1,21 @@
+local config = function()
+  require("oil").setup({
+    default_file_explorer = true,
+  })
+end
+
+return {
+  'stevearc/oil.nvim',
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {},
+  -- Optional dependencies
+  dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+  lazy = true,
+  event = "VimEnter",
+  config = config,
+  keys = {
+    { "<leader>fe", "<cmd>Oil<CR>", desc = "File Explorer" }
+  },
+}
