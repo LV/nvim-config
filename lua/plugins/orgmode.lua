7cf9b9efedcf9058e82c6426356701238b005fab
Luis Victoria <v@lambda.lv>
2024-10-20 01:09:48 -0400

    Move `orgmode` config to its own function

--- lua/plugins/orgmode.lua
+++ lua/plugins/orgmode.lua
@@ -1,19 +1,26 @@
+-- List of default settings:
+-- https://github.com/nvim-orgmode/orgmode/blob/master/lua/orgmode/config/defaults.lua
+
+local config = function()
+  -- Setup orgmode
+  require('orgmode').setup({
+    org_agenda_files = '~/org/**/*',
+    org_default_notes_file = '~/org/refile.org',
+    org_hide_emphasis_markers = false,
+    org_hide_leading_stars = false,
+  })
+
+  -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
+  -- add ~org~ to ignore_install
+  -- require('nvim-treesitter.configs').setup({
+  --   ensure_installed = 'all',
+  --   ignore_install = { 'org' },
+  -- })
+end
+
 return {
   'nvim-orgmode/orgmode',
   event = 'VeryLazy',
   ft = { 'org' },
-  config = function()
-    -- Setup orgmode
-    require('orgmode').setup({
-      org_agenda_files = '~/org/**/*',
-      org_default_notes_file = '~/org/refile.org',
-    })
-
-    -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
-    -- add ~org~ to ignore_install
-    -- require('nvim-treesitter.configs').setup({
-    --   ensure_installed = 'all',
-    --   ignore_install = { 'org' },
-    -- })
-  end,
+  config = config,
 }
