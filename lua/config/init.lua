d906f9898109532db4bb732c4fce442e6d57a619
Luis Victoria <v@lambda.lv>
2024-10-21 14:46:16 -0400

    Rename `autocmds.lua` -> `indentation.lua`

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -5,8 +5,8 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("config.autocmds")
 require("config.globals")
+require("config.indentation")
 require("config.keymaps")
 require("config.options")
 
