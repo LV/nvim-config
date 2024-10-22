0904135b8ca63b4a2faa60300aca96edd0c91a18
Luis Victoria <v@lambda.lv>
2024-10-22 17:20:18 -0400

    Apply `Esc` keybindings only when in shell

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,6 +11,29 @@ mapkey("<leader>wj", "wincmd j", "n") -- Navigate down
 mapkey("<leader>wh", "wincmd h", "n") -- Navigate left
 mapkey("<leader>wl", "wincmd l", "n") -- Navigate right
 
+
 -- Go to normal mode from terminal mode by pressing Escape
-vim.api.nvim_set_keymap("t", "<Escape>", "<C-\\><C-n>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("t", "<Insert><Escape>", "<Escape>", { noremap = true, silent = true })
+  -- Apply this only when in the shell
+    -- i.e. do not overwrite keybindings when in terminal mode for other commands such as LazyGit
+local function apply_shell_mappings()
+  -- List of common shell programs
+  local shells = { "bash", "zsh", "sh", "fish" }
+
+  -- Get the terminal's command (buffer name)
+  local term_cmd = vim.fn.expand("%:p:t")
+
+  -- if the terminal buffer is running a shell, apply custom mappings
+  if vim.tbl_contains(shells, term_cmd) then
+    vim.api.nvim_set_keymap("t", "<Escape>", "<C-\\><C-n>", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("t", "<Insert><Escape>", "<Escape>", { noremap = true, silent = true })
+  else
+    -- For non-shell terminal applications, revert to default escape behavior
+    vim.api.nvim_set_keymap("t", "<Escape>", "<Escape>", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("t", "<Insert><Escape>", "<Insert><Escape>", { noremap = true, silent = true })
+  end
+end
+
+vim.api.nvim_create_autocmd("TermOpen", {
+  pattern = "*",
+  callback = apply_shell_mappings
+})
