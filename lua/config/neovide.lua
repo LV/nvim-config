433dbf35bcbd5a4a7b0638dbcd3e7dc448cfc5bb
lvictoria7 <lvictoria7@bloomberg.net>
2024-10-22 15:44:12 -0400

    Add TODO on bug with `fzf` and `ripgrep` when searching

--- lua/config/neovide.lua
+++ lua/config/neovide.lua
@@ -8,7 +8,7 @@ vim.g.neovide_transparency = 1.0 -- TODO: Make it so that transparency is 1.0 if
 if vim.g.neovide then
   vim.api.nvim_create_autocmd("VimEnter", {
     callback = function()
-      vim.cmd("silent! cd ~")
+      vim.cmd("silent! cd ~") -- TODO: This kind of breaks all the other buffer stuff! Can't ripgrep or fzf from the file's respective directory anymore
     end
   })
 end
