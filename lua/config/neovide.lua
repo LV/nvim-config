83d8cb66435aed48a9757e3023010ab699d55da8
lvictoria7 <v@lambda.lv>
2024-10-22 13:47:51 -0400

    Automatically `cd` into `~` when launching neovide (for WSL)

--- lua/config/neovide.lua
+++ lua/config/neovide.lua
@@ -1,4 +1,14 @@
 -- Docs: https://neovide.dev/configuration.html
 
 vim.o.guifont = "PragmataPro Mono Liga:h19"
-vim.g.neovide_transparency = 0.75
+vim.g.neovide_transparency = 1.0
+
+
+-- Override configuration variables if launching on Neovide
+if vim.g.neovide then
+  vim.api.nvim_create_autocmd("VimEnter", {
+    callback = function()
+      vim.cmd("silent! cd ~")
+    end
+  })
+end
