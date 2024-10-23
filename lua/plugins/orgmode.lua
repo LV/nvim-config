ad52c15fc3778e3da9e315521441c1a48802dd22
Luis Victoria <v@lambda.lv>
2024-10-23 18:10:39 -0400

    Show all items in `.org` document

--- lua/plugins/orgmode.lua
+++ lua/plugins/orgmode.lua
@@ -5,11 +5,12 @@ local config = function()
   -- Overwrite default settings
   -- https://github.com/nvim-orgmode/orgmode/blob/master/lua/orgmode/config/defaults.lua
   require('orgmode').setup({
+    org_adapt_indentation = false,
     org_agenda_files = '~/org/**/*',
     org_default_notes_file = '~/org/refile.org',
     org_hide_emphasis_markers = false,
     org_hide_leading_stars = false,
-    org_adapt_indentation = false,
+    org_startup_folded = "showeverything", -- Do not collapse elements upon launching
   })
 
   -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
