d3387f8cfda63c0b16f0818bda9fa5fd0ece3a22
Luis Victoria <v@lambda.lv>
2024-10-22 23:33:33 -0400

    Remove unknown type in `oil.nvim`

--- lua/plugins/oil-nvim.lua
+++ lua/plugins/oil-nvim.lua
@@ -14,7 +14,6 @@ end
 return {
   'stevearc/oil.nvim',
   ---@module 'oil'
-  ---@type oil.SetupOpts
   opts = {},
   -- Optional dependencies
   dependencies = { { "echasnovski/mini.icons", opts = {} } },
