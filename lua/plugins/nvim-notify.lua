fee706fa1c9d8fca7b2e6ecab67cb35de6ebdc6f
Luis Victoria <v@lambda.lv>
2024-11-04 20:24:14 -0500

    "Fix" `background_color` typo
    This is actually the incorrect way of spelling 'color'. British people
    do this all the time.

--- lua/plugins/nvim-notify.lua
+++ lua/plugins/nvim-notify.lua
@@ -2,7 +2,7 @@ local config = function()
   require("notify").setup({
     stages = "fade_in_slide_out",
     timeout = 3000,
-    background_color = "#000000",
+    background_colour = "#000000",
     render = "default",
   })
   vim.notify = require("notify")
