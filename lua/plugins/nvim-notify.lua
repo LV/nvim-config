e87247c3bfd32619d72bfb8190a0c55d460ac6c1
Luis Victoria <v@lambda.lv>
2024-11-04 20:16:00 -0500

    Add `nvim-notify`

---
+++ lua/plugins/nvim-notify.lua
@@ -0,0 +1,15 @@
+local config = function()
+  require("notify").setup({
+    stages = "fade_in_slide_out",
+    timeout = 3000,
+    background_color = "#000000",
+    render = "default",
+  })
+  vim.notify = require("notify")
+end
+
+return {
+  "rcarriga/nvim-notify",
+  event = "VeryLazy",
+  config = config,
+}
