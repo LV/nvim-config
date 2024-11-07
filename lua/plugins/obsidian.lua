e038cb048593e5ac20481b7ba0a54d17ab73d025
Luis Victoria <v@lambda.lv>
2024-11-06 20:36:22 -0500

    Create atomic note template

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -46,28 +46,7 @@ return {
     },
 
     -- `true` indicates that you don't want obsidian.nvim to manage frontmatter.
-    disable_frontmatter = false,
-
-    -- Optional, alternatively you can customize the frontmatter data.
-    ---@return table
-    note_frontmatter_func = function(note)
-      -- Add the title of the note as an alias.
-      if note.title then
-        note:add_alias(note.title)
-      end
-
-      local out = { id = get_note_id(note.title), aliases = note.aliases, tags = note.tags }
-
-      -- `note.metadata` contains any manually added fields in the frontmatter.
-      -- So here we just make sure those fields are kept in the frontmatter.
-      if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
-        for k, v in pairs(note.metadata) do
-          out[k] = v
-        end
-      end
-
-      return out
-    end,
+    disable_frontmatter = true,
 
     -- Optional, customize how note IDs are generated given an optional title.
     ---@param title string|?
@@ -77,10 +56,6 @@ return {
     end,
 
     notes_subdir = "Notes",
-
-    templates = {
-      folder = "Templates",
-    },
 
     workspaces = {
       {
