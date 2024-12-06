dd4217e5560731e8d9cf1acb4d916e423177a3d5
Luis Victoria <v@lambda.lv>
2024-12-06 05:37:48 -0500

    Add `render-markdown` pkg

---
+++ lua/plugins/render-markdown.lua
@@ -0,0 +1,17 @@
+local config = function()
+  require('obsidian').setup({
+    ui = { enable = false },
+  })
+end
+
+return {
+  'MeanderingProgrammer/render-markdown.nvim',
+  dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+  ---@module 'render-markdown'
+  ---@type render.md.UserConfig
+  opts = {},
+  config = config,
+  ft = { "markdown" },
+}
