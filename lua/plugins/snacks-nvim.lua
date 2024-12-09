1894bd360e0f5307091a699071b2a3073adbdea0
Luis Victoria <v@lambda.lv>
2024-12-09 13:10:20 -0500

    Add a group for `git blame`

--- lua/plugins/snacks-nvim.lua
+++ lua/plugins/snacks-nvim.lua
@@ -27,17 +27,20 @@ return {
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
     bigfile = { enabled = true },
+    lazygit = {
+
+    },
     notifier = { enabled = true },
     quickfile = { enabled = true },
     statuscolumn = { enabled = true },
     words = { enabled = true },
   },
   keys = {
-    { "<leader>.",  function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
-    { "<leader>os", function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
-    { "<leader>gB", function() Snacks.gitbrowse() end, desc = "Git Browse" },
-    { "<leader>gg", function() lazygit_repo_from_cwd() end, desc = "LazyGit" },
-    { "<leader>gl", function() Snacks.git.blame_line() end, desc = "Git Blame Line" },
-    { "<leader>t",  function() Snacks.terminal() end, desc = "Toggle Terminal" },
+    { "<leader>.",   function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
+    { "<leader>os",  function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
+    { "<leader>gB",  function() Snacks.gitbrowse() end, desc = "Git Browse" },
+    { "<leader>gg",  function() lazygit_repo_from_cwd() end, desc = "LazyGit" },
+    { "<leader>gbl", function() Snacks.git.blame_line() end, desc = "Git Blame Line" },
+    { "<leader>t",   function() Snacks.terminal() end, desc = "Toggle Terminal" },
   },
 }
