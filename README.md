0d55da172f88cab6c53287ee8bdc4e7d353ce77c
Luis Victoria <v@lambda.lv>
2024-12-10 11:43:49 -0500

    Add GitHub dashboard

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # LV's NVim Config
 
 ## Pre-requisites
+### MacOS (Homebrew)
 ```sh
-brew install basedpyright cpplint fzf ripgrep vscode-langservers-extracted
+brew install basedpyright cpplint fzf gh hub ripgrep vscode-langservers-extracted
 ```
+
+### Additional Items
+```sh
+gh ext install meiji163/gh-notify
+```
+
+Also install the following:
+- [shell-color-scripts](https://gitlab.com/dwt1/shell-color-scripts/-/tree/master?ref_type=heads)
--- lua/plugins/snacks-nvim.lua
+++ lua/plugins/snacks-nvim.lua
@@ -20,10 +20,11 @@ local lazygit_repo_from_cwd = function()
 end
 
 
+-- CONFIGURATIONS
 ---@class snacks.dashboard.Config
+---@field enabled? boolean
 ---@field sections snacks.dashboard.Section
 ---@field formats table<string, snacks.dashboard.Text|fun(item:snacks.dashboard.Item, ctx:snacks.dashboard.Format.ctx):snacks.dashboard.Text>
--- CONFIGURATIONS
 local dashboard_config = {
   enabled = true,
   preset = {
@@ -36,7 +37,80 @@ local dashboard_config = {
 ╚══════╝   ╚═╝   
     ]]
   },
-  -- TODO: Add the GitHub dashboard
+  sections = {
+    { section = "header" },
+    {
+      pane = 2,
+      section = "terminal",
+      cmd = "colorscript -e square",
+      height = 5,
+      padding = 1,
+    },
+    { section = "keys", gap = 1, padding = 1 },
+    {
+      pane = 2,
+      icon = " ",
+      desc = "Browse Repo",
+      padding = 1,
+      key = "b",
+      action = function()
+        Snacks.gitbrowse()
+      end,
+    },
+    function()
+      local in_git = Snacks.git.get_root() ~= nil
+      local cmds = {
+        {
+          title = "Notifications",
+          cmd = "gh notify -s -a -n5",
+          action = function()
+            vim.ui.open("https://github.com/notifications")
+          end,
+          key = "n",
+          icon = " ",
+          height = 5,
+          enabled = true,
+        },
+        {
+          title = "Open Issues",
+          cmd = "gh issue list -L 3",
+          key = "i",
+          action = function()
+            vim.fn.jobstart("gh issue list --web", { detach = true })
+          end,
+          icon = " ",
+          height = 7,
+        },
+        {
+          icon = " ",
+          title = "Open PRs",
+          cmd = "gh pr list -L 3",
+          key = "p",
+          action = function()
+            vim.fn.jobstart("gh pr list --web", { detach = true })
+          end,
+          height = 7,
+        },
+        {
+          icon = " ",
+          title = "Git Status",
+          cmd = "hub --no-pager diff --stat -B -M -C",
+          height = 10,
+        },
+      }
+      return vim.tbl_map(function(cmd)
+        return vim.tbl_extend("force", {
+          pane = 2,
+          section = "terminal",
+          enabled = in_git,
+          padding = 1,
+          ttl = 5 * 60,
+          indent = 3,
+        }, cmd)
+      end, cmds)
+    end,
+    { section = "startup" },
+  },
 }
 
 
