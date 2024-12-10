ae3f4aa09fc996a1df940b1fd47ad3b9e4dfe677
Luis Victoria <v@lambda.lv>
2024-12-10 12:06:53 -0500

    Add descriptions for window navigation

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,14 +2,14 @@ local mapkey = require("util.keymapper").mapvimkey
 -- mapvimkey(keymaps, command, vimmode, options)
 
 -- Pane and Window Navigation
-mapkey("<leader>wk", "<C-w>k", "n") -- Navigate up
-mapkey("<leader>wj", "<C-w>j", "n") -- Navigate down
-mapkey("<leader>wh", "<C-w>h", "n") -- Navigate left
-mapkey("<leader>wl", "<C-w>l", "n") -- Navigate right
-mapkey("<leader>wk", "wincmd k", "n") -- Navigate up
-mapkey("<leader>wj", "wincmd j", "n") -- Navigate down
-mapkey("<leader>wh", "wincmd h", "n") -- Navigate left
-mapkey("<leader>wl", "wincmd l", "n") -- Navigate right
+mapkey("<leader>wk", "<C-w>k", "n", { desc = "Navigate Up" })
+mapkey("<leader>wj", "<C-w>j", "n", { desc = "Navigate Down" })
+mapkey("<leader>wh", "<C-w>h", "n", { desc = "Navigate Left" })
+mapkey("<leader>wl", "<C-w>l", "n", { desc = "Navigate Right" })
+mapkey("<leader>wk", "wincmd k", "n", { desc = "Navigate Up" })
+mapkey("<leader>wj", "wincmd j", "n", { desc = "Navigate Down" })
+mapkey("<leader>wh", "wincmd h", "n", { desc = "Navigate Left" })
+mapkey("<leader>wl", "wincmd l", "n", { desc = "Navigate Right" })
 
 
 -- Go to normal mode from terminal mode by pressing Escape
