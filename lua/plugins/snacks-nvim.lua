687cd0d2e6a715f0f9365ec77711e612e40e7411
Luis Victoria <lvictoria7@bloomberg.net>
2024-12-10 11:48:16 -0500

    Add smooth scrolling

--- lua/plugins/snacks-nvim.lua
+++ lua/plugins/snacks-nvim.lua
@@ -127,6 +127,7 @@ return {
     indent = { enabled = true },
     notifier = { enabled = true },
     quickfile = { enabled = true },
+    scroll = { enabled = true },
     statuscolumn = { enabled = true },
     words = { enabled = true },
   },
