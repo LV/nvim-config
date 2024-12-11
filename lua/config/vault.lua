5134a65fe38c24458c643d8517a02d92ba2fe5f3
Luis Victoria <v@lambda.lv>
2024-12-11 02:30:17 -0500

    Change `VaultPath` depending on OS's `hostname`

--- lua/config/vault.lua
+++ lua/config/vault.lua
@@ -1,2 +1,18 @@
 -- Configuration options for obsidian vault
-VaultPath = "~/bbvault"
+local function get_hostname()
+  local handle = io.popen("hostname") -- Runs the hostname command
+  if handle then
+    local hostname = handle:read("*a"):gsub("\n", "") -- Removes the newline
+    handle:close()
+    return hostname
+  else
+    return nil
+  end
+end
+
+local hostname = get_hostname()
+if hostname == "lvictoria7" then
+  VaultPath = "~/bbvault"
+else
+  VaultPath = "~/vault"
+end
