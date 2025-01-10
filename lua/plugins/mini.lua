b935fa9c8524cd88e8208a0b79222c332f1dcb03
Luis Victoria <v@lambda.lv>
2025-01-09 19:36:07 -0500

    Add basic options in `mini.starter`

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -39,10 +39,35 @@ local jump2d = function()
 end
 
 local starter = function()
-  require("mini.starter").setup()
-  -- Ensure it launches at startup if no file is provided
+  -- Debug mappings
+  vim.opt.timeout = true
+  vim.opt.timeoutlen = 0  -- Set to 0 to eliminate waiting for multi-key sequences
+
+  require("mini.starter").setup({
+    evaluate_single = true,
+
+    items = {
+      { name = "Find", action = "Telescope find_files", section = "Actions" },
+      { name = "Grep", action = "Telescope live_grep", section = "Actions" },
+      { name = "Explorer", action = "lua MiniFiles.open()", section = "Actions" },
+      { name = "Quit", action = "q", section = "Actions" },
+    },
+  })
+
+  -- Add an autocommand to check mappings in starter buffer
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = "starter",
+    callback = function()
+      -- Print all 'g' mappings in the buffer
+      vim.cmd("verbose map g")
+
+      -- Force immediate recognition of 'g' key
+      vim.keymap.set('n', 'g', 'g', { buffer = true, nowait = true })
+    end
+  })
+
   if vim.fn.argc() == 0 then
-    vim.cmd("enew") -- Open a new empty buffer
+    vim.cmd("enew")
     require("mini.starter").open()
   end
 end
