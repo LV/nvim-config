1e652269652b99b87a751e4a0bd11069a5d9efe0
Luis Victoria <v@lambda.lv>
2025-01-15 22:43:14 -0500

    Remove `starter` and add `surround`

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -34,46 +34,12 @@ local jump2d = function()
   })
 end
 
-local starter = function()
-  -- Debug mappings
-  vim.opt.timeout = true
-  vim.opt.timeoutlen = 0  -- Set to 0 to eliminate waiting for multi-key sequences
-
-  require("mini.starter").setup({
-    evaluate_single = true,
-
-    items = {
-      { name = "Explorer", action = "Oil", section = "Actions" },
-      { name = "Find", action = "Telescope find_files", section = "Actions" },
-      { name = "Grep", action = "Telescope live_grep", section = "Actions" },
-      { name = "New",  action = "enew", section = "Actions" },
-      { name = "Quit", action = "q", section = "Actions" },
-    },
-
-    header = "",
-    footer = "",
-  })
-
-  -- Add an autocommand to check mappings in starter buffer
-  vim.api.nvim_create_autocmd("FileType", {
-    pattern = "starter",
-    callback = function()
-      -- Print all 'g' mappings in the buffer
-      vim.cmd("verbose map g")
-
-      -- Force immediate recognition of 'g' key
-      vim.keymap.set('n', 'g', 'g', { buffer = true, nowait = true })
-    end
-  })
-
-  if vim.fn.argc() == 0 then
-    vim.cmd("enew")
-    require("mini.starter").open()
-  end
-end
-
 local statusline = function()
   require("mini.statusline").setup()
+end
+
+local surround = function()
+  require("mini.surround").setup()
 end
 
 return {
@@ -85,7 +51,7 @@ return {
     indentscope()
     jump()
     jump2d()
-    starter()
     statusline()
+    surround()
   end,
 }
