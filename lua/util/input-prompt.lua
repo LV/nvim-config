ab0ab10a8ca78421ff18bb74ea1c9faa73ad0750
Luis Victoria <lvictoria7@bloomberg.net>
2025-04-02 17:17:57 -0400

    Add type annotations to `PromptInput`

--- lua/util/input-prompt.lua
+++ lua/util/input-prompt.lua
@@ -1,3 +1,5 @@
+---@param promptDescription string
+---@return string
 function PromptInput(promptDescription)
   local input_text
   Snacks.input.input({ prompt = string.format("%s", promptDescription) }, function(input)
