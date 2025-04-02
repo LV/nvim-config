2b87f9ed55e2ab7b3afb128eb3fed9d86049eaaa
Luis Victoria <lvictoria7@bloomberg.net>
2025-04-02 17:17:33 -0400

    Remove `config/autocmds.lua`

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -5,7 +5,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("config.autocmds")
 require("config.globals")
 require("config.indentation")
 require("config.keymaps")
